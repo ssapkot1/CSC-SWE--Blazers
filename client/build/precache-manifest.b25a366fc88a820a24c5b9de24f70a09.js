8704364af35444264e82e8fca8c9661c8398c2c9
profbunbun <aaronrls1984@gmail.com>
2023-11-27 12:34:07 -0500

    made build

---
+++ client/build/precache-manifest.b25a366fc88a820a24c5b9de24f70a09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb7eef3145a2168887c794689eaf728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b62a77b8aff81878ed37",
+    "url": "/static/css/2.cc82f8aa.chunk.css"
+  },
+  {
+    "revision": "9a48c2a490f948302b74",
+    "url": "/static/css/main.c1fab54f.chunk.css"
+  },
+  {
+    "revision": "b62a77b8aff81878ed37",
+    "url": "/static/js/2.712aa04d.chunk.js"
+  },
+  {
+    "revision": "b2207ffa4f9b2724a0152cc60bf0e71c",
+    "url": "/static/js/2.712aa04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a48c2a490f948302b74",
+    "url": "/static/js/main.db3af155.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
