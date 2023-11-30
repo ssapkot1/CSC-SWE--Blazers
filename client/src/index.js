16ee6c89b6f80cc626d91e47fa98cf281b078ba7
muhammad uzair <uzair@plutus21.com>
2023-11-30 03:27:19 -0500

    service worker added again

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // create the root
@@ -13,3 +14,4 @@ root.render(
     </BrowserRouter>
 );
 
+serviceWorker.unregister();
