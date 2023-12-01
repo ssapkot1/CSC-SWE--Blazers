a9ef3eeacac608a8aace9e9fd49dc1532b6d8a87
BunBun <73380142+profbunbun@users.noreply.github.com>
2023-12-01 15:33:39 -0500

    reroute

--- client/src/components/search.component.js
+++ client/src/components/search.component.js
@@ -20,7 +20,7 @@ const SearchComponent = () => {
   // New function to handle clicking on a search result
   const onResultClick = (movieId) => {
     console.log(movieId)
-    history.push(`https://blazeback.onrender.com/movies/details/${movieId}`); // Redirect to the movie details page
+    history.push(`/movies/details/${movieId}`); // Redirect to the movie details page
   };
 
   return (
